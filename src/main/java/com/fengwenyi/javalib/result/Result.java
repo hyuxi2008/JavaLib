c2581652bac346d7ecad30adc358790db2b106dd
fengwenyi <xfsy_2015@163.com>
2018-06-15 09:32:55 +0800

    更新Result

--- src/main/java/com/fengwenyi/javalib/result/Result.java
+++ src/main/java/com/fengwenyi/javalib/result/Result.java
@@ -6,14 +6,31 @@ package com.fengwenyi.javalib.result;
  */
 public class Result {
 
+    /** 返回码 */
     private int code;
+
+    /** 返回码说明 */
     private String msg;
+
+    /** 数据 */
     private Object data;
 
     /**
      * 实例化
      */
     public Result() {
+    }
+
+    /**
+     * 创建对象
+     * @param code 返回码
+     * @param msg  信息
+     * @param data 数据
+     */
+    public Result(int code, String msg, Object data) {
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
     }
 
     // [common]
@@ -35,6 +52,17 @@ public class Result {
      * @param iReturnCode 自定义枚举类，需要实现或继承IReturnCode
      * @param data 自定义数据对象
      */
+    public void setResult(IReturnCode iReturnCode, Object data) {
+        common(iReturnCode);
+
+        this.data = data;
+    }
+
+    /**
+     * 添加数据
+     * @param iReturnCode 自定义枚举类，需要实现或继承IReturnCode
+     * @param data 自定义数据对象
+     */
     public void setData(IReturnCode iReturnCode, Object data) {
         common(iReturnCode);
 
@@ -48,5 +76,29 @@ public class Result {
                 ", msg='" + msg + '\'' +
                 ", data=" + data +
                 '}';
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
     }
 }
